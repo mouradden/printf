bb1657938cd35e37bf60d035edebdd927a712e42
mourad denguir <denguirmourad0@gmail.com>
2023-06-26 14:04:42 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int	_printf(const char *format, ...)
 	i = 0;
 	if (!format)
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	va_start(args, format);
 	while (format[i])
 	{
